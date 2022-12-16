a28ca81514c633f6b5963988d5f542ee06a018ae
Rahul Dutta <rdtech2002@gmail.com>
2022-12-16 11:52:53 +0530

    .

---
+++ Assignment 16-12-2022/p_02_pyramid.java
@@ -0,0 +1,13 @@
+// nasted for loop
+// pyramid
+public class p_02_pyramid {
+
+  public static void main(String[] args) {
+    for (int i = 0; i < 5; i++) {
+      for (int j = 0; j < i; j++) {
+        System.out.print("* ");
+      }
+      System.out.println("");
+    }
+  }
+}
