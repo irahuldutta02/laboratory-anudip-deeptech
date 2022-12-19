3ac77c597c783911b3105ab8be14bebd5b50a527
Rahul Dutta <rdtech2002@gmail.com>
2022-12-19 11:49:39 +0530

    .

--- Assignment 19-12-2022/Calculator.java
+++ Assignment 19-12-2022/Calculator.java
@@ -13,42 +13,48 @@ public class Calculator {
         );
         System.out.print("Enter your choice : ");
         choice = S.next().charAt(0);
-        switch (choice) {
-          case '1':
-            System.out.print("Enter two numbers : ");
-            a = S.nextFloat();
-            b = S.nextFloat();
-            res = a + b;
-            System.out.print("Result : " + res);
-            break;
-          case '2':
-            System.out.print("Enter two numbers : ");
-            a = S.nextFloat();
-            b = S.nextFloat();
-            res = a - b;
-            System.out.print("Result : " + res);
-            break;
-          case '3':
-            System.out.print("Enter two numbers : ");
-            a = S.nextFloat();
-            b = S.nextFloat();
-            res = a / b;
-            System.out.print("Result : " + res);
-            break;
-          case '4':
-            System.out.print("Enter two numbers : ");
-            a = S.nextFloat();
-            b = S.nextFloat();
-            res = a * b;
-            System.out.print("Result : " + res);
-            break;
-          case '5':
-            System.exit(0);
-            break;
-          default:
-            System.out.print("Wrong Choice.....\n");
-            break;
-        }
+				switch(choice)
+				{ 
+				    case '1':System.out.print("Enter two numbers : ");
+				    a=S.nextFloat();
+				    b=S.nextFloat();	
+				    res=a+b;
+				    System.out.print("Result : " +res);
+				    break;
+				     
+				     case '2':System.out.print("Enter two numbers : ");
+					 a=S.nextFloat();
+					 b=S.nextFloat();	
+					 res=a-b;
+					 System.out.print("Result : " +res);
+					 break;
+					    
+				     case '3':System.out.print("Enter two numbers : ");
+					 a=S.nextFloat();
+					 b=S.nextFloat();	
+					 res=a/b;
+					 System.out.print("Result : " +res);
+					 break;
+					    
+				     case '4':System.out.print("Enter two numbers : ");
+					 a=S.nextFloat();
+					 b=S.nextFloat();	
+					 res=a*b;
+					 System.out.print("Result : " +res);
+					 break;
+					 
+				     case'5': System.out.print("Enter two numbers : ");
+					 a=S.nextFloat();
+					 b=S.nextFloat();	
+					 res=a%b;
+					 System.out.print("Result : " +res);
+					   
+				     case '6':
+					 System.exit(0);
+					 break;
+					 default : System.out.print("Wrong Choice.....\n");
+					 break;
+				}
       } while (choice != 5);
     }
   }
